8f11ed73206af165a5a30b733a88648877ccbf8a
Raquel Bayas <31921518+RaquelBayas@users.noreply.github.com>
2024-02-04 19:18:38 +0100

    Deploying

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
-import express from "express";
-import cors from "cors";
-import jwt from "jsonwebtoken";
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 import { foods, sample_users, tags } from "./data";
 
 const app = express();
